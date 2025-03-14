a7546cca5a565e204e5dfbd8daccf59d09f9d255
0x-onigiri <ipfworldpeace@gmail.com>
2025-03-15 07:02:31 +0900

    chore: format with stylistic

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Button } from "@/components/ui/button";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Button } from '@/components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [
+    count,
+    setCount,
+  ] = useState(0)
 
   return (
     <>
@@ -18,15 +21,17 @@ function App() {
       </div>
       <h1 className="text-blue-400 font-bold text-2xl">Vite + React</h1>
       <div>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <Button onClick={() => setCount(count => count + 1)}>
+          count is
+          {' '}
+          {count}
         </Button>
         <a href="/sample" className="block">
           Go to Sample Page
         </a>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,18 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route  } from "react-router";
-import "./index.css";
-import App from "./App.tsx";
-import Sample from "./Sample.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!)
+createRoot(document.getElementById('root')!)
   .render(
     <StrictMode>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/sample" element={<Sample />} />
         </Routes>
       </BrowserRouter>
     </StrictMode>,
-  );
+  )
