933a47f025ac208f35a85b8fab675dfbd4377102
0x-onigiri <ipfworldpeace@gmail.com>
2025-04-24 21:07:17 +0900

    update contract constant

--- packages/frontend/src/constants/contract.ts
+++ packages/frontend/src/constants/contract.ts
@@ -1,7 +1,7 @@
 export const SHAKE_ONIGIRI = {
   testnet: {
-    packageId: '0x4a3b25024d3df95a594968093b6579a667ff352e69f3a755edf65e9b9e3802d1',
-    userListObjectId: '0xd312fc2bfe0bb95bc4e973d8769b1e488aefe22b63909da9d5501f2d5daa5267',
-    postPaymentObjectId: '0x880609e9196f1ccafd18782f458e49c092518cadbca62c372b4f005e335b16bf',
+    packageId: '0xcb908ae115c642d580b26e7ca8d014223493d07535e31d4255cd731c7e50901d',
+    userListObjectId: '0x9964a7e08bdc4f57a403dbf6e9395ec67ff0960c2f9b1180d478f32ebbb2b688',
+    postPaymentObjectId: '0x3bbb2e336e667bbfb84d564e729ef5e8afe1da4eb222b5787edf7befd8efcc97',
   },
 }
